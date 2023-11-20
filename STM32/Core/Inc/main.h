33bb626562bb39f45c80daa3bfe1f529357fcb0e
justpassby <vipproga123gunny@gmail.com>
2023-11-20 14:44:08 +0700

    update

--- STM32/Core/Inc/main.h
+++ STM32/Core/Inc/main.h
@@ -58,6 +58,10 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
+#define LED_YELLOW_Pin GPIO_PIN_5
+#define LED_YELLOW_GPIO_Port GPIOA
+#define TIM3_CH1_Pin GPIO_PIN_6
+#define TIM3_CH1_GPIO_Port GPIOA
 /* USER CODE BEGIN Private defines */
 
 /* USER CODE END Private defines */
